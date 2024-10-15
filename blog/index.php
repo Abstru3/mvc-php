4cde1bc210580d686421e23855eb11c4c0566045
KORAQI <besjan.koraqii@gmail.com>
2024-10-15 09:30:24 +0200

    Isolez l'acces aux donees

--- blog/index.php
+++ blog/index.php
@@ -1,28 +1,5 @@
 <?php
-// Connexion à la base de données
-try {
-    $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
-} catch (Exception $e) {
-    die('Erreur : ' . $e->getMessage());
-}
-
-// Récupération des 5 derniers billets
-$statement = $database->query(
-    "SELECT id, titre, contenu, DATE_FORMAT(date_creation, '%d/%m/%Y à %Hh%imin%ss') AS date_creation_fr FROM billets ORDER BY date_creation DESC LIMIT 0, 5"
-);
-
-$posts = []; // Initialisation du tableau
-
-while ($row = $statement->fetch()) {
-    $post = [
-        'title' => $row['titre'],
-        'french_creation_date' => $row['date_creation_fr'],
-        'content' => $row['contenu'],
-    ];
-    $posts[] = $post; // Ajout du post au tableau
-}
-
-// Inclusion du fichier template pour afficher les données
+require('src/model.php');
+$posts = getPosts();
 require('templates/homepage.php');
-
 ?>
--- blog/templates/homepage.php
+++ blog/templates/homepage.php
@@ -8,19 +8,25 @@
 <body>
     <h1>Le super blog de l'AVBN !</h1>
     <p>Derniers billets du blog :</p>
-
-    <?php foreach ($posts as $post) { ?>
-        <div class="news">
-            <h3>
-                <?php echo htmlspecialchars($post['title']); ?>
-                <em>le <?php echo $post['french_creation_date']; ?></em>
-            </h3>
-            <p>
-                <?php echo nl2br(htmlspecialchars($post['content'])); ?>
-                <br />
-                <em><a href="#">Commentaires</a></em>
-            </p>
-        </div>
-    <?php } // Fin de la boucle des billets ?>
+    <?php
+    foreach ($posts as $post) {
+    ?>
+    <div class="news">
+        <h3>
+            <?php echo htmlspecialchars($post['title']); ?>
+            <em>le <?php echo $post['french_creation_date']; ?></em>
+        </h3>
+        <p>
+            <?php
+            // Afficher le contenu du billet
+            echo nl2br(htmlspecialchars($post['content']));
+            ?>
+            <br />
+            <em><a href="#">Commentaires</a></em>
+        </p>
+    </div>
+    <?php
+    } // Fin de la boucle des billets
+    ?>
 </body>
 </html>
