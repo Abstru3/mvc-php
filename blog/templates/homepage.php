826b759561cb98c491b4f9494f3ee56a0b1e21ae
KORAQI <besjan.koraqii@gmail.com>
2024-10-22 10:14:11 +0200

    Creer un template et un layout

--- blog/templates/homepage.php
+++ blog/templates/homepage.php
@@ -7,25 +7,32 @@
     </head>
 
     <body>
-        <h1>Le super blog de l'AVBN !</h1>
-        <p>Derniers billets du blog :</p>
-
+        <?php $title = "Le blog de l'AVBN"; ?>
+        <?php ob_start(); ?>
+            <h1>Le super blog de l'AVBN !</h1>
+            <p>Derniers billets du blog :</p>
         <?php
-        foreach ($posts as $post) {
-        ?>
+            foreach ($posts as $post) {
+            ?>
             <div class="news">
                 <h3>
-                    <?= htmlspecialchars($post['title']); ?>
-                    <em>le <?= $post['french_creation_date']; ?></em>
+                <?= htmlspecialchars($post['title']); ?>
+                <em>le <?= $post['frenchCreationDate']; ?> </em>
                 </h3>
                 <p>
-                    <?= nl2br(htmlspecialchars($post['content'])); ?>
-                    <br />
-                    <em><a href="post.php?id=<?= urlencode($post['identifier']) ?>">Commentaires</a></em>
+                <?= nl2br(htmlspecialchars($post['content'])); ?>
+                <br />
+                <em>
+                <a href="post.php?id=<?= urlencode($post['identifier']) ?> ">
+                Commentaires
+                </a></em>
                 </p>
             </div>
-        <?php
-        }
-        ?>
+            <?php
+            }
+            ?>
+        <?php $content = ob_get_clean(); ?>
+        <?php require('layout.php') ?>
+        
     </body>
 </html>
