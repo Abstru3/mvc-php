dd80378e46f2f7d692eb62fd85ec32e42c371c2a
KORAQI <besjan.koraqii@gmail.com>
2024-10-22 09:02:48 +0200

    Base pour afficher les commentaires

--- blog/src/model.php
+++ blog/src/model.php
@@ -1,26 +1,25 @@
 <?php
+
 function getPosts() {
-    // Connexion à la base de données
     try {
         $database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
-    } catch (Exception $e) {
-        die('Erreur : ' . $e->getMessage());
+    } catch(Exception $e) {
+        die('Erreur : '.$e->getMessage());
     }
-    
-    // Récupération des 5 derniers billets
+
     $statement = $database->query(
-        "SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS creation_date_fr FROM posts ORDER BY creation_date DESC LIMIT 0, 5"
+        "SELECT id, title, content, DATE_FORMAT(creation_date, '%d/%m/%Y à %Hh%imin%ss') AS french_creation_date FROM posts ORDER BY creation_date DESC LIMIT 0, 5"
     );
-    
     $posts = [];
-    while ($row = $statement->fetch()) {
+    while (($row = $statement->fetch())) {
         $post = [
             'title' => $row['title'],
-            'creation_date_fr' => $row['creation_date_fr'],
+            'french_creation_date' => $row['french_creation_date'],
             'content' => $row['content'],
         ];
+
         $posts[] = $post;
     }
-    
+
     return $posts;
 }
